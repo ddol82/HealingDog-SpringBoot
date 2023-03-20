fb339a0b8ea09a87c5ef5ae7f092f580c8b7e242
ddol82 <wlssud@mensakorea.org>
2023-03-20 13:46:32 +0900

    first commit to initialize project

---
+++ src/main/java/com/healing/healingdog/HealingdogApplication.java
@@ -0,0 +1,13 @@
+package com.healing.healingdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingdogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingdogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healing/healingdog/HealingdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healing.healingdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
