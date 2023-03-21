6417a88d54c0f822c2c5e9338021ae9316f89daa
cymon337 <113357179+cymon337@users.noreply.github.com>
2023-03-21 14:44:13 +0900

    feat(위탁돌봄관리(boardingManage) API 작성중): 위탁돌봄관리(boardingManage) API 작성중
    - 컨트롤러 생성
    - 서비스 생성
    - common/ResponseDTO 클래스 생성

---
+++ src/main/java/com/healing/healingdog/boarding/controller/BoardingManageController.java
@@ -0,0 +1,28 @@
+package com.healing.healingdog.boarding.controller;
+
+import com.healing.healingdog.boarding.service.BoardingManageService;
+import com.healing.healingdog.common.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/boarding-manage")
+@RequiredArgsConstructor
+public class BoardingManageController {
+
+    private final BoardingManageService boardingManageService;
+
+//  위탁돌봄 관리페이지 접속 시 모든 정보 불러옴
+    @GetMapping("/")
+    public ResponseEntity<ResponseDTO> selectBoarding(int providerCode) {
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "갤러리 조회 성공", boardingManageService.selectBoarding(providerCode)));
+    }
+
+
+}
---
+++ src/main/java/com/healing/healingdog/boarding/service/BoardingManageService.java
@@ -0,0 +1,19 @@
+package com.healing.healingdog.boarding.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BoardingManageService {
+
+
+
+
+
+    public Object selectBoarding(int providerCode) {
+        return "";
+    }
+}
