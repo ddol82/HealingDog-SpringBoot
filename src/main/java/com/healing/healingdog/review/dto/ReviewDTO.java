0cb48e39d838838b7949e505118235dff2a6a35a
cymon337 <113357179+cymon337@users.noreply.github.com>
2023-03-24 15:51:18 +0900

    feat(reviews 리스트 조회): 리뷰목록조회 작성완료

---
+++ src/main/java/com/healing/healingdog/review/dto/ReviewDTO.java
@@ -0,0 +1,22 @@
+package com.healing.healingdog.review.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewDTO {
+
+    private int reviewsCode;
+    private int serviceCategoryCode;
+    private int providerCode;
+    private int userCode;
+    private Timestamp registDate;
+    private String content;
+    private int score;
+
+}
