10ccab3b88344b9dcfd599dd75094322d6f72d17
spacekingdom <spacekingdom99@gmail.com>
2023-03-27 14:55:50 +0900

    beauty insert 기능

--- src/main/java/com/healing/healingdog/beauty/controller/BeautyManageController.java
+++ src/main/java/com/healing/healingdog/beauty/controller/BeautyManageController.java
@@ -1,5 +1,6 @@
 package com.healing.healingdog.beauty.controller;
 
+import com.healing.healingdog.beauty.model.dto.BeautyDTO;
 import com.healing.healingdog.beauty.model.service.BeautyManageService;
 import com.healing.healingdog.common.ResponseDTO;
 import lombok.RequiredArgsConstructor;
@@ -8,9 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @Slf4j
 @RestController
-@RequestMapping("api/v1/beauty-manage")
+@RequestMapping("/api/v1/beauty-manage")
 @RequiredArgsConstructor
 public class BeautyManageController {
     private final BeautyManageService beautyManageService;
@@ -18,7 +21,44 @@ public class BeautyManageController {
     /**
      * 미용실관리 페이지 접속 시 모든정보 불러옴
      * */
+    @GetMapping("/")
     public ResponseEntity<ResponseDTO> selectBeauty(int providerCode){
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"미용실 조회 성공", beautyManageService.selectBeauty(providerCode)));
+    }
+
+    /**
+     * 미용실관리 미용실 상세정보 조회
+     * */
+    @GetMapping("/info")
+    public ResponseEntity<ResponseDTO> selectBeautyInfo(@RequestBody HashMap<String,String> input){
+        log.info("REQUEST API selectBeautyInfo ={}", input);
+        int providerCode = Integer.parseInt(input.get("providerCode"));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "미용실 정보조회 성공", beautyManageService.selectBeautyInfo(providerCode)));
+    }
+
+    /**
+     * 미용실관리 미용실정보 등록
+     * */
+    @PostMapping("/info")
+    public ResponseEntity<ResponseDTO> registerBeautyInfo(BeautyDTO beautyDTO){
+        log.info("REQUEST API registBeautyInfo ={}",beautyDTO);
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"미용실 정보등록 성공", (beautyManageService.registerBeautyInfo(beautyDTO)) + "개"));
+    }
+
+    /**
+     * 미용실관리 미용실정보 수정
+     * */
+    public ResponseEntity<ResponseDTO> updateBeautyInfo(BeautyDTO beautyDTO){
+        log.info("REQUEST API updateBeautyInfo ={}", beautyDTO);
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "미용실 정보수정 성공", (beautyManageService.updateBeautyInfo(beautyDTO)) + "개"));
+    }
+
+    /**
+     * 미용실관리 미용실정보 삭제
+     * */
+    public ResponseEntity<ResponseDTO> deleteBeautyInfo(@RequestBody HashMap<String, String> input){
+        log.info("REQUEST API deleteBeautyInfo ={}",input);
+        int providerCode = Integer.parseInt(input.get("providerCode"));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"미용실 정보삭제 성공",(beautyManageService.deleteBeautyInfo(providerCode)) + "개"));
     }
 }
--- src/main/java/com/healing/healingdog/beauty/model/service/BeautyManageService.java
+++ src/main/java/com/healing/healingdog/beauty/model/service/BeautyManageService.java
@@ -1,6 +1,7 @@
 package com.healing.healingdog.beauty.model.service;
 
 import com.healing.healingdog.beauty.model.dao.BeautyManageMapper;
+import com.healing.healingdog.beauty.model.dto.BeautyDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,45 @@ public class BeautyManageService {
 
     public Object selectBeauty(int providerCode) {
         return "result";
+    }
+
+    /**
+     *  미용실 정보조회
+     * */
+    public Object selectBeautyInfo(int providerCode){
+        log.info("REQUEST SERVICE selectBeautyInfo = {}", providerCode);
+        BeautyDTO result = beautyManageMapper.selectBeautyInfo(providerCode);
+        log.info("result = {}", result.toString());
+        return result;
+    }
+
+    /**
+     *  미용실 정보 등록
+     * */
+    public int registerBeautyInfo(BeautyDTO beautyDTO) {
+        log.info("REQUEST SERVICE registerBeautyInfo = {}",beautyDTO);
+        int result = beautyManageMapper.registerBeautyInfo(beautyDTO);
+        log.info("result.toString() = {}", result + "개 등록 완료");
+        return result;
+    }
+
+    /**
+     *  미용실 정보 수정
+     * */
+    public int updateBeautyInfo(BeautyDTO beautyDTO) {
+        log.info("REQUEST SERVICE updateBeautyInfo = {}",beautyDTO);
+        int result = beautyManageMapper.updateBeautyInfo(beautyDTO);
+        log.info("result,toString() ={}", result +"개 수정 완료");
+        return result;
+    }
+
+    /**
+     *  미용실 정보 삭제
+     * */
+    public int deleteBeautyInfo(int providerCode) {
+        log.info("REQUEST SERVICE deleteBeautyInfo = {}", providerCode);
+        int result = beautyManageMapper.deleteBeautyInfo(providerCode);
+        log.info("result.toString() ={}", result +"개 삭제 완료.");
+        return result;
     }
 }
