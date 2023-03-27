d23b3d6735d81eb723e128b47c1b37fb3272501c
김선중 <sjkim3515@gmail.com>
2023-03-27 14:53:55 +0900

    03-27 중간 커밋푸쉬 사용자,제공자 로그인완료 사용자 기본입력 회원가입완료

---
+++ src/main/java/com/healing/healingdog/login/dao/AuthMapper.java
@@ -0,0 +1,23 @@
+package com.healing.healingdog.login.dao;
+
+import com.healing.healingdog.login.dto.ProviderDTO;
+import com.healing.healingdog.login.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
+
+@Mapper
+public interface AuthMapper {
+
+    //이메일 중복조회
+    UserDTO selectByEmail(String email);
+
+    //회원가입
+    int insertUser(UserDTO user);
+
+    //로그인
+    Optional<UserDTO> findByUserEmail(String email);
+
+    //로그인
+    Optional<ProviderDTO> findByProviderEmail(String email);
+}
