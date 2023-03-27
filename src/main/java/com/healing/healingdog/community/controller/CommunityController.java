93019a4b41ef3192df8db7f9b21aea057db9809a
ddol82 <wlssud@mensakorea.org>
2023-03-27 09:49:47 +0900

    🦄 refactor: pull을 위한 중간 커밋
    pull을 위해 중간 커밋을 합니다.

--- src/main/java/com/healing/healingdog/community/controller/CommunityController.java
+++ src/main/java/com/healing/healingdog/community/controller/CommunityController.java
@@ -1,6 +1,8 @@
 package com.healing.healingdog.community.controller;
 
 import com.healing.healingdog.common.ResponseDTO;
+import com.healing.healingdog.common.paging.PageData;
+import com.healing.healingdog.common.paging.PageDataAutoFill;
 import com.healing.healingdog.community.model.service.CommunityService;
 import com.healing.healingdog.community.model.type.BoardType;
 import lombok.extern.slf4j.Slf4j;
@@ -35,25 +37,30 @@ public class CommunityController {
 
     /**
      * 게시판 리스트를 조회합니다.<br>
-     * Mapping : [<b>GET</b>] {@code /lists/boards?cat=[]&page=[]}
+     * Mapping : [<b>GET</b>] {@code /lists/boards?cat={}&page={}}
      *
      * @param categoryType 카테고리의 타입은 영어 소문자로만 구성되어있습니다.
      * @param currPage 현재 페이지를 입력받습니다.
      * @return 조건을 만족하는 게시글의 리스트를 출력합니다.<br>
      * 카테고리 값이 비정상적인 경우 <b>전체 게시글</b>을 조회합니다.<br>
-     *
+     * 현재 페이지 값이 비정상적인 경우 <b>1</b>페이지로 이동합니다.
      */
     @GetMapping("/lists/boards")
     public ResponseEntity<ResponseDTO> selectBoardList(@RequestParam(name="cat", defaultValue = "all") String categoryType,
                                           @RequestParam(name="page", defaultValue = "1") int currPage) {
         log.info("[CommunityController] selectBoardList 호출");
-        log.debug("(selectBoardList) categoryType : " + categoryType);
-        log.debug("(selectBoardList) currPage : " + currPage);
+        log.trace("(selectBoardList) categoryType : " + categoryType);
+        log.trace("(selectBoardList) currPage : " + currPage);
 
         BoardType boardType = BoardType.getBoardType(categoryType);
         log.debug("(selectBoardList) boardType : {}", boardType);
 
         int boardCountAll = communityService.selectBoardCountAll(boardType.getCode());
+        PageData pageData = PageDataAutoFill.get(currPage, boardCountAll);
+        log.debug("(selectBoardList) pageData filled : {}", pageData);
+        if(currPage < 1 || currPage > pageData.getEndPage()) {
+            currPage = 1;
+        }
 
         return ResponseEntity.ok()
                 .body(new ResponseDTO(HttpStatus.OK, "a", boardCountAll));
