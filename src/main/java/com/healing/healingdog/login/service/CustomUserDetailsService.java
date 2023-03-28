54cd494a251f95f3c0800bce882fe00a397187e6
김선중 <sjkim3515@gmail.com>
2023-03-28 09:52:18 +0900

    03-28 아침 커밋푸쉬 사용자,제공자 로그인완료 사용자 기본입력 회원가입완료 제공자 기본 회원가입 거진완료

--- src/main/java/com/healing/healingdog/login/service/CustomUserDetailsService.java
+++ src/main/java/com/healing/healingdog/login/service/CustomUserDetailsService.java
@@ -1,8 +1,8 @@
 package com.healing.healingdog.login.service;
 
 
-import com.healing.healingdog.exception.UserNotFoundException;
 import com.healing.healingdog.login.dao.AuthMapper;
+import com.healing.healingdog.login.dto.ProviderDTO;
 import com.healing.healingdog.login.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -27,9 +27,30 @@ public class CustomUserDetailsService implements UserDetailsService {
         log.info("[CustomUserDetailsService] ===================================");
         log.info("[CustomUserDetailsService] loadUserByUsername {}", email);
 
-        return mapper.findByUserEmail(email)
-                .map(user -> addAuthorities(user))
-                .orElseThrow(() -> new UserNotFoundException(email + "> 찾을 수 없습니다."));
+        UserDetails userDetails = null;//UserDetails 을 null 로 선언
+
+
+        userDetails = mapper.findByUserEmail(email)
+                .map(this::addAuthorities)
+                .orElse(null);
+        if(userDetails != null) {
+            return userDetails; // findByUserEmail이 있으면 리턴한다
+        }
+        //아니면 findByProviderEmail 을 찾는다
+        userDetails = mapper.findByProviderEmail(email)
+                .map(this::addAuthorities)
+                .orElse(null);
+        if(userDetails == null) { // findByUserEmail, findByProviderEmail 두개다 없으면 UsernameNotFoundException을 던진다
+            throw new UsernameNotFoundException(email + "에 해당하는 user 또는 provider를 찾을 수 없습니다."); //UserDetails이 비어있으면  UsernameNotFoundException 을 날린다
+        }
+        return userDetails;
+
+    }
+
+    private ProviderDTO addAuthorities(ProviderDTO providerDTO) {
+        providerDTO.setAuthorities(Arrays.asList(new SimpleGrantedAuthority(providerDTO.getRole())));
+
+        return providerDTO;
     }
 
     private UserDTO addAuthorities(UserDTO userDto) {
@@ -37,5 +58,8 @@ public class CustomUserDetailsService implements UserDetailsService {
 
         return userDto;
     }
+
+
+
 
 }
